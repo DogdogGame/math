2046866e1694ab9977cf9b13cde90b47e8888c97
Wenqiang Li <yyhaxx@163.com>
2019-04-17 22:30:01 +0800

    feat(vector): Arithmetic Operations

--- main.vector.py
+++ main.vector.py
@@ -5,3 +5,11 @@ if __name__ == "__main__":
   print(vec)
   print(len(vec))
   print("vec[0]={}, vec[1]={}".format(vec[0], vec[1]))
+
+  vec2 = Vector([4, 7])
+  print("{} + {} = {}".format(vec, vec2, vec + vec2))
+  print("{} - {} = {}".format(vec, vec2, vec - vec2))
+  print("{} * {} = {}".format(vec, 4, vec * 4))
+  print("{} * {} = {}".format(4, vec, 4 * vec))
+  print("+{} = {}".format(vec, +vec))
+  print("-{} = {}".format(vec, -vec))
--- vector.py
+++ vector.py
@@ -1,6 +1,6 @@
 class Vector:
   def __init__ (self, lst):
-    self._values = lst
+    self._values = list(lst)
   def __repr__(self):
     return "Vector({})".format(self._values)
   def __str__(self):
@@ -9,3 +9,27 @@ class Vector:
     return len(self._values)
   def __getitem__(self, index):
     return self._values[index]
+  # 迭代器
+  def __iter__(self):
+    return self._values.__iter__()
+  # 加法
+  def __add__(self, another):
+    assert len(self) == len(another), \
+      "向量维度需要相同"
+    return Vector([a + b for a, b in zip(self, another)])
+  # 减法
+  def __sub__(self, another):
+    assert len(self) == len(another), \
+      "向量维度需要相同"
+    return Vector([a - b for a, b in zip(self, another)])
+  # 乘法
+  def __mul__(self, k):
+    return Vector([k * e for e in self])
+  def __rmul__(self, k):
+    return self * k
+  # 取正
+  def __pos__(self):
+    return 1 * self
+  # 取负
+  def __neg__(self):
+    return -1 * self
